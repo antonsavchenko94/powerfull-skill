44f951abc95dd7d5007c17dedb25c7db8f4f15c2
antonsavchenko94 <savchenko.inc@gmail.com>
2016-11-03 02:23:34 +0200

    add new publication(delete, update) word

--- imports/api/lists/methods.js
+++ imports/api/lists/methods.js
@@ -1,6 +1,6 @@
 import {Meteor} from "meteor/meteor";
 import {FlowRouter} from "meteor/kadira:flow-router";
-import {Dictionaries} from "../models";
+import {Dictionaries, Words} from "../models";
 import {check} from "meteor/check";
 
 Meteor.methods({
@@ -23,14 +23,14 @@ Meteor.methods({
 
     'user.changeUsername'(username) {
         check(username, String);
-        if(this.userId) {
+        if (this.userId) {
             Accounts.setUsername(this.userId, username)
         }
     },
 
     'user.changeEmail'(email) {
         check(email, String);
-        if(this.userId) {
+        if (this.userId) {
             let currentUserEmail = Meteor.user().emails[0];
             Accounts.removeEmail(this.userId, currentUserEmail);
             Accounts.addEmail(this.userId, email);
@@ -38,17 +38,17 @@ Meteor.methods({
     },
 
     'user.saveAvatar'(imageBase64Url) {
-        check(imageBase64Url, String)
-        if(this.userId){
+        check(imageBase64Url, String);
+        if (this.userId) {
             Meteor.users.update({_id: this.userId}, {
-                $set:{
+                $set: {
                     avatarUrl: imageBase64Url
                 }
             }, (err, doc) => {
-                if(err) {
-                    console.log(err)
-                }else {
-                    console.log(doc)
+                if (err) {
+                    Meteor.Error(err);
+                } else {
+                    console.log(doc);
                 }
             })
         }
@@ -66,7 +66,7 @@ Meteor.methods({
         };
         Dictionaries.insert(dictionary, (err, doc) => {
             if (err) {
-                console.log(err)
+                Meteor.Error(err);
             }
         })
     },
@@ -76,40 +76,66 @@ Meteor.methods({
         if (this.userId) {
             Dictionaries.remove(id, (err, doc) => {
                 if (err) {
-                    console.log(err)
+                    Meteor.Error(err);
                 } else {
                     console.log(`Document ${doc} was removed`)
                 }
             })
         }
-
     },
 
-    'save.word'(word, dictionaryId) {
+    'dictionary.update'(id, modifier){
+        check(id, String);
+        check(modifier, Object);
+        if (this.userId) {
+            Dictionaries.update({_id: id}, {
+                    $set: modifier
+                },
+                (err, doc) => {
+                    if (err) {
+                        Meteor.Error(err);
+                    } else {
+                        console.log(`${doc} document was updated`)
+                    }
+                })
+        }
+    },
+
+    'word.save'(word) {
         check(word, {
+            dictionaryId: String,
             spell: String,
             transcription: String,
             translation: String,
+            context: Match.Maybe(String),
             checked: Boolean
         });
-        Dictionaries.update(dictionaryId, {
-            $push: {
-                words: word
-            }
-        }, (err, doc) => {
+        if (this.userId) {
+            word.userId = this.userId;
+            Words.insert(word, (err, doc) => {
+                if (err) {
+                    Meteor.Error(err);
+                }
+            })
+        }
+    },
+
+    'word.delete'(id){
+        check(id, String);
+        Words.remove({_id: id}, (err, doc) => {
             if (err) {
-                Meteor.Error(err)
+                Meteor.Error(err);
             }
         })
     },
 
-    'word.setChecked'(id, word, setChecked){
+    'word.setChecked'(id, setChecked){
         check(id, String);
         check(setChecked, Boolean);
 
-        Dictionaries.update({_id: id, 'words.spell': word}, {
+        Words.update({_id: id}, {
             $set: {
-                'words.$.checked': setChecked
+                checked: setChecked
             }
         }, (err, doc)=> {
             if (err) {
