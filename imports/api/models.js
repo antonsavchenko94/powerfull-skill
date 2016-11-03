4a274d528d092f4b3377284cc9fd93c9d7e879b4
antonsavchenko94 <savchenko.inc@gmail.com>
2016-11-03 02:26:30 +0200

    decompose dictionaries

--- imports/api/models.js
+++ imports/api/models.js
@@ -2,4 +2,6 @@ import {Mongo} from "meteor/mongo";
 
 const Dictionaries = new Mongo.Collection('dictionaries');
 
-export {Dictionaries}
+const Words = new Mongo.Collection('words');
+
+export {Dictionaries, Words}
